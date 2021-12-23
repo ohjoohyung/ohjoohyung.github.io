507b5e9f9eb1a6da706ea649a7017cc4900f7d47
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-12-23 15:37:23 +0900

    feat: 노션에 있던 네트워크 책 정리 블로그로 이전

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -1,7 +1,7 @@
 import Typography from 'typography';
 
 const typography = new Typography({
-  baseFontSize: '16px',
+  baseFontSize: '15px',
   baseLineHeight: 1.666,
   headerFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
   bodyFontFamily: ['Apple SD Gothic Neo', 'Nanum Gothic'],
